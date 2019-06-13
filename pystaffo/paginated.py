30e63422cd854e377cb1c2a0b29147e7f2f4fc34
Lorenzo Capelli <l.capelli@stuart.com>
2019-06-13 17:57:36 +0100

    new paginated_0.2

--- pystaffo/paginated.py
+++ pystaffo/paginated.py
@@ -13,22 +13,20 @@ def get(auth=None, url=None, extras=None):
     params = {'page': page, 'per_page': per_page}
     if extras:
         params.update(extras)
-    try:
-        while keep_going: # keep iterating over pages until `keep_going == false`
-            params.update({'page': page}) # first request with page=1
-            # API request
-            r = requests.get(url=url, auth=auth, params=params, )
-            response = json.loads(r.content.decode('utf-8'))
-            # if the API returns an empty array. we know we dont have any data left
-            # so we can safely abort
-            if len(response) == 0:
-                keep_going = False
-            if (not 'Page' in r.headers):
-                data += response
-                keep_going = False
-            else:
-                data += response
-                page += 1
-    except KeyError:
-        pass
+    r = requests.get(url=url, auth=auth, params=params)
+    response = json.loads(r.content.decode('utf-8'))
+    data = response
+    if len(response) == 0 or (not 'Page' in r.headers): # if the API returns an empty array or no pages, we know we dont have any data left
+        keep_going = False
+    while keep_going: # keep iterating over pages until `keep_going == false`
+        page += 1
+        params.update({'page': page}) 
+        r = requests.get(url=url, auth=auth, params=params)
+        response = json.loads(r.content.decode('utf-8'))
+        if len(response) == 0:
+            keep_going = False
+        else:
+            data += response
     return data
+
+    
